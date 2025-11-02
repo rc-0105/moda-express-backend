dd0dc46e7ab8c5529e8d35e25ccb6f58d5781b21
rc-0105 <rcarrero@unbosque.edu.co>
2025-11-02 04:52:48 -0500

    Initial build

---
+++ src/main/java/com/modaexpress/modaexpress_backend/ModaexpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.modaexpress.modaexpress_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModaexpressBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModaexpressBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/modaexpress/modaexpress_backend/ModaexpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modaexpress.modaexpress_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModaexpressBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
